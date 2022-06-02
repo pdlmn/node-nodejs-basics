1410ea0a99b21e81e141fee7d666c3ffcce2c6a0
pdlmn <djeinwings@gmail.com>
2022-06-03 00:16:32 +0200

    feat: add list.js & read.js solutions

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,24 @@
+import fs from 'fs/promises'
+import path from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const PATH_TO_FILE = path.join(__dirname, 'files')
+
+const access = async (path) => await fs.access(path)
+    .then(() =>  true)
+    .catch(() => false)
+
 export const list = async () => {
-    // Write your code here 
+  const isExist = await access(PATH_TO_FILE)
+
+  if (!isExist) {
+    throw new Error('FS operation failed')
+  }
+
+  const files = await fs.readdir(PATH_TO_FILE)
+  files.forEach(file => console.log(file))
 };
+
+list()
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,24 @@
+import fs from 'fs/promises'
+import path from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const PATH_TO_FILE = path.join(__dirname, 'files/fileToRead.txt')
+
+const access = async (path) => await fs.access(path)
+    .then(() =>  true)
+    .catch(() => false)
+
 export const read = async () => {
-    // Write your code here 
+  const isExist = await access(PATH_TO_FILE)
+
+  if (!isExist) {
+    throw new Error('FS operation failed')
+  }
+
+  const content = await fs.readFile(PATH_TO_FILE)
+  console.log(content.toString())
 };
+
+read()
