5f402506526f2899d1bc0a9a259c0bbbe1b09362
pdlmn <djeinwings@gmail.com>
2022-06-02 23:17:19 +0200

    feat: add access utility function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,12 @@
+import fs from 'fs/promises'
+import path from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const PATH_TO_FILES = path.join(__dirname, 'files')
+const PATH_TO_FILES_COPY = path.join(__dirname, 'files_copy')
+
 export const copy = async () => {
-    // Write your code here 
+  
 };
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,19 +1,29 @@
 import fs from 'fs/promises'
-import { fileURLToPath } from 'url'
 import path from 'path'
-
+import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+
 const CONTENT = 'I am fresh and young'
 const PATH_TO_FILE = path.join(__dirname, 'files/fresh.txt')
 
+const access = async (path) => {
+  let result
+  await fs.access(path)
+    .then(() => result = true)
+    .catch(() => result = false)
+
+  return result
+}
+
 export const create = async () => {
-  const access = await fs.access(PATH_TO_FILE)
-    .then(() => true)
-    .catch(() => false)
-  if (!access) {
+  const pathExists = await access(PATH_TO_FILE)
+
+  if (!pathExists) {
     await fs.writeFile(PATH_TO_FILE, CONTENT)
   } else {
     throw new Error('FS operation failed')
   }
 };
+
+create()
