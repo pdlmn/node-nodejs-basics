91b2a4cf8d2c2c36fe1b2195bc98e15d665666a1
pdlmn <djeinwings@gmail.com>
2022-06-03 00:10:00 +0200

    feat: add delete.js solution

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,23 @@
+import fs from 'fs/promises'
+import path from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const PATH_TO_FILE = path.join(__dirname, 'files/fileToRemove.txt')
+
+const access = async (path) => await fs.access(path)
+    .then(() =>  true)
+    .catch(() => false)
+
 export const remove = async () => {
-    // Write your code here 
+  const isExist = await access(PATH_TO_FILE)
+
+  if (!isExist) {
+    throw new Error('FS operation failed')
+  }
+
+  fs.rm(PATH_TO_FILE)
 };
+
+remove()
