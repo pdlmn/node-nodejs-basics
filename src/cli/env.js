2f3ce95236bd1639a4b42d2ffb4bd9795accc2e4
pdlmn <djeinwings@gmail.com>
2022-06-04 13:12:11 +0200

    feat: add env.js solution

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,19 @@
 export const parseEnv = () => {
-    // Write your code here 
+  // get all entries with RSS_ at the start
+  const envVars = Object.entries(process.env)
+    .filter(envVar => envVar[0].slice(0, 4) === 'RSS_')
+
+  let result = ''
+  for (let i = 0; i < envVars.length; i++) {
+    const [key, value] = envVars[i]
+    result += `${key}=${value}`
+    // if the item is last
+    if (i < envVars.length - 1) {
+      result += '; '
+    }
+  }
+
+  console.log(result)
 };
+
+parseEnv()
