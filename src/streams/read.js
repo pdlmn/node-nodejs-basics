80b4c7299fd31769c1ac3e5fb61465320260f1d1
pdlmn <djeinwings@gmail.com>
2022-06-04 14:36:55 +0200

    feat: add read.js solution

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,15 @@
+import { ReadStream } from 'fs'
+import path from 'path'
+
+const __filename = process.argv[1]
+const __dirname = path.dirname(__filename)
+
 export const read = async () => {
-    // Write your code here 
+  const stream = ReadStream(path.join(__dirname, './files/fileToRead.txt'))
+  stream.on('data', (data) => {
+    process.stdout.write(data.toString())
+    process.stdout.write('\n')
+  })
 };
+
+read()
