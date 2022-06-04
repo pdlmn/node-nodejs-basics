8844191e3daadb7394868026dc39786ea372f10b
pdlmn <djeinwings@gmail.com>
2022-06-04 12:57:42 +0200

    feat: add args.js solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,17 @@
 export const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2)
+  let result = ''
+  let i = 0
+  while (i < args.length) {
+    const key = args[i].slice(2)
+    const value = args[i + 1]
+    if (value) {
+      result += `${key} is ${value}, `
+    }
+
+    i += 2
+  }
+  console.log(result.slice(0, -2))
 };
+
+parseArgs()
