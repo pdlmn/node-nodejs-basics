0d793190c93976d3facf26c9608ebdfaa57f7d95
pdlmn <djeinwings@gmail.com>
2022-06-05 13:56:26 +0200

    feat: add transform.js solution

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,22 @@
+import { Transform } from 'stream';
+import { pipeline } from 'stream/promises';
+
 export const transform = async () => {
-    // Write your code here 
+  await pipeline(
+    process.stdin,
+    new Transform({
+      transform(chunk, enc, cb) {
+        const reversed = chunk
+          .toString()
+          .trim()
+          .split('')
+          .reverse()
+          .join('')
+        cb(null, reversed + '\n')
+      }
+    }),
+    process.stdout
+  )
 };
+
+transform()
