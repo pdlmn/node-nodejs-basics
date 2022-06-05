a7aa5f7feca7fd60ef66ed0f48f46342edeb9a13
pdlmn <djeinwings@gmail.com>
2022-06-05 13:56:10 +0200

    fix: remake write.js solution with streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,14 @@
+import path from 'path'
+import { appendFile } from 'fs/promises'
+import { createReadStream, createWriteStream } from 'fs'
+import { stdout } from 'process';
+
+const __filename = process.argv[1]
+const __dirname = path.dirname(__filename)
+
 export const write = async () => {
-    // Write your code here 
+  const stream = createWriteStream(path.join(__dirname, './files/fileToWrite.txt'))
+  process.stdin.pipe(stream)
 };
+
+write()
