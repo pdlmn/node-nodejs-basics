06cb010093b0c525473a3b493e03a9988a482d35
pdlmn <djeinwings@gmail.com>
2022-06-05 13:55:47 +0200

    fix: fix read.js solution

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,15 +1,14 @@
-import { ReadStream } from 'fs'
+import { createReadStream } from 'fs'
 import path from 'path'
+import { finished } from 'stream';
 
 const __filename = process.argv[1]
 const __dirname = path.dirname(__filename)
 
 export const read = async () => {
-  const stream = ReadStream(path.join(__dirname, './files/fileToRead.txt'))
-  stream.on('data', (data) => {
-    process.stdout.write(data.toString())
-    process.stdout.write('\n')
-  })
+  const stream = createReadStream(path.join(__dirname, './files/fileToRead.txt'))
+  stream.pipe(process.stdout)
+  finished(stream, () => { process.stdout.write('\n') })
 };
 
 read()
